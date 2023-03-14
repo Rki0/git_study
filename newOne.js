3d486cda285e013ac77549efdb0e9d639da666d7
Rki0 <pky00823@gmail.com>
2023-03-14 11:10:44 +0900

    feat(utils): add function for header

---
+++ newOne.js
@@ -0,0 +1,3 @@
+const newOne = () => {
+  return "this is new file. main branch doesn't have this file.";
+};
---
+++ utils/hi.js
@@ -0,0 +1,5 @@
+const hi = () => {
+  const str = "this is new file and folder.";
+
+  return "hi";
+};
